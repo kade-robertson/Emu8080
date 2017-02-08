f3e0d06ca405d075891d5f960423496b9382c3cf
kade-robertson <kade.robertson96@gmail.com>
2017-02-08 11:53:49 -0500

    add CPUBus object to handle interrupt and in/out events

---
+++ Emu8080/Objects/CPUBus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emu8080
+{
+    public class CPUBus 
+    {
+        public delegate void InterruptChangedHandler(CPUBus bus, EventArgs e);
+        public delegate void InputRequestedHandler(CPUBus bus, EventArgs e);
+        public delegate void OutputReceivedHandler(CPUBus bus, EventArgs e);
+
+        public event InterruptChangedHandler InterruptChanged;
+        public event InputRequestedHandler InputRequested;
+        public event OutputReceivedHandler OutputReceived;
+
+        private bool m_interrupt = false;
+
+        public bool Interrupt {
+            get {
+                return m_interrupt; 
+            }
+            set {
+                m_interrupt = value;
+                InterruptChanged?.Invoke(this, null);
+            }
+        }
+
+        public void RequestInput() {
+            InputRequested?.Invoke(this, null);
+        }
+
+        public void DeliverOutput() {
+            OutputReceived?.Invoke(this, null);
+        }
+    }
+}
